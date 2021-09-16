cdebdb89c3b48e3a174fa6d4712ec944d1e41f7f
Rander Carlos <randerccf@gmail.com>
2021-09-16 18:50:04 -0300

    add routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::group(['prefix' => 'v1'], function () {
     Route::post('auth/login', 'AuthController@login')->name('login');
@@ -20,11 +8,14 @@ Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'auth', 'middleware' => 'auth:api'], function () {
         Route::post('logout', 'AuthController@logout')->name('logout');
         Route::post('refresh-token', 'AuthController@refreshToken')->name('refreshToken');
-        Route::post('logged-user', 'AuthController@loggedUser')->name('loggedUser');
+        Route::post('me', 'AuthController@me')->name('me');
     });
 
     Route::middleware('auth:api')->group(function () {
-        Route::apiResource('jobs', 'JobController');
+        Route::apiResource('users', 'UsersController');
+        Route::apiResource('employees', 'EmployeesController');
+        Route::apiResource('employees.addresses', 'AddressesController');
+        Route::apiResource('employees.salaries', 'EmployeesSalariesHistoricController')->only(['index', 'store', 'destroy']);
     });
 });
 
