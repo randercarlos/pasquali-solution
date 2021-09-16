1927478e9173d4e360262d3f70a28f18457bb9d0
Rander Carlos <randerccf@gmail.com>
2021-09-16 18:43:56 -0300

    improve global exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,6 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
