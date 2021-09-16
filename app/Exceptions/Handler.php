21c6715d39eb3675be33d74ca88b72a33ddd2f43
Rander Carlos <randerccf@gmail.com>
2021-09-16 08:40:33 -0300

    change exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,43 +47,23 @@ class Handler extends ExceptionHandler
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
-     */
     public function render($request, Throwable $exception)
     {
         if ($exception instanceof AuthenticationException) {
-            return response()->json([
-                'msg' => 'Unauthenticated. It\'s necessary to be authenticated to access this endpoint'
-            ], 401);
+            return response()->fail('Unauthenticated. It\'s necessary to be authenticated to access this endpoint', 401);
 
         } else if ($exception instanceof UnauthorizedException) {
-            return response()->json([
-                'msg' => $exception->getMessage() ?? "This user doesn't have permission to perform this action."
-            ], 403);
+            return response()->fail($exception->getMessage() ?? "This user doesn't have permission to perform this action.", 403);
 
         } else if ($exception instanceof ValidationException) {
-            return response()->json([
-                'msg' => $exception->errors()
-            ], 422);
+            return response()->fail($exception->errors(), 422);
 
         } else if ($exception instanceof ModelNotFoundException) {
-
-            return response()->json([
-                'error' => class_basename($exception->getModel()) . ' not found!'
-            ], 404);
+            return response()->fail('Model not found!', 404);
 
         } else if ($exception instanceof QueryException) {
             if ($exception->errorInfo[1] === 1062) {
-                return response()->json([
-                    'error' => 'A record already exists with this field and can\'t be duplicate!'
-                ], 422);
+                return response()->fail('A record already exists with this field and can\'t be duplicate!', 422);
             }
         }
 
@@ -98,15 +78,9 @@ TEXT;
         Log::debug($errorDescription);
 
         if (App::environment() === 'production') {
-            return response()->json([
-                'error' => 'An error occurred. Check the logs for more information.'
-            ], 500);
-
+            return response()->fail('An error occurred. Check the logs for more information.', 500);
         } else {
-            return response()->json([
-                'error' => $exception
-            ], 500);
-
+            return response()->fail($exception->getMessage(), 500);
         }
 
         return parent::render($request, $exception);
