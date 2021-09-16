92a9b65598c39c1f78ebe1626524f83c2034fe00
Rander Carlos <randerccf@gmail.com>
2021-09-15 22:01:24 -0300

    create ResponseMacroServiceProvider

---
+++ app/Providers/ResponseMacroServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\ServiceProvider;
+
+class ResponseMacroServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Response::macro('success', function($data, $statusCode = 200) {
+            return response()->json( $array = [
+                'success' => true,
+                'data' => $data
+            ], $statusCode);
+        });
+
+        Response::macro('fail', function($msg, $statusCode) {
+            return response()->json( [
+                'success' => false,
+                'msg' => $msg
+            ], $statusCode);
+        });
+    }
+}
